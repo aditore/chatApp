c49b48e2b7b858d144af5038eb5cedeb414599e8
aditore <anthonyditore@gmail.com>
2022-07-24 13:53:51 -0700

    adding react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import {
-  Join
+  Join,
 } from "./components/socket";
+import {
+  Startup
+} from "./pages";
 
 
 function App() {
   return (
-    <>
-      <Join />
-    </>
+    <Router>
+      <>
+        <main>
+        <Routes>
+          <Route index element={<Startup />} />
+          <Route path="join" element={<Join />} />
+        </Routes>
+        </main>
+      </>
+    </Router>
   )
 }
 
